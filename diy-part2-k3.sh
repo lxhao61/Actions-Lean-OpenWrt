1941355f014c50df2e49c47ebe738084b0ba249e
haoran <lxhao61@gmail.com>
2025-05-23 19:43:18 +0800

    Update diy-part2-k3.sh

--- diy-part2-k3.sh
+++ diy-part2-k3.sh
@@ -22,10 +22,6 @@ rm -rf feeds/packages/lang/golang
 # 拉取 golang
 git clone https://github.com/sbwml/packages_lang_golang.git -b 24.x feeds/packages/lang/golang
 
-# 删除自带 v2ray-geodata
-#rm -rf feeds/packages/net/v2ray-geodata
-#rm -rf package/feeds/packages/v2ray-geodata
-
 # 删除自带 xray-core
 rm -rf feeds/packages/net/xray-core
 rm -rf package/feeds/packages/xray-core
